96d17bf1a3f868acae4f2e27134a917d895b1024
殷正韬 <steven.yin@smart.com>
2026-04-01 21:23:23 +0800

    updating

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -2,7 +2,7 @@ use migration::Migrator;
 use sea_orm::{Database, DatabaseConnection, DbBackend, Statement};
 use tokio::net::TcpListener;
 use z2p_axum::configuration::DatabaseSettings;
-
+use wiremock::MockServer;
 use sea_orm_migration::MigratorTrait;
 use z2p_axum::{configuration::get_configuration, startup::run};
 
@@ -11,7 +11,13 @@ use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
-    pub db_pool: sea_orm::DatabaseConnection,
+    pub email_server: MockServer,
+    pub port: u16,
+}
+
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
 }
 
 pub async fn spawn_app() -> TestApp {
@@ -20,7 +26,7 @@ pub async fn spawn_app() -> TestApp {
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-
+    let email_server = MockServer::start().await;
     let mut configuration = get_configuration().expect("Failed to read configuration.");
 
     // 为每个测试生成唯一数据库名
@@ -33,10 +39,10 @@ pub async fn spawn_app() -> TestApp {
     tokio::spawn(async move {
         server.await.expect("Failed to run server");
     });
-
     TestApp {
         address,
-        db_pool: db,
+        email_server,
+        port
     }
 }
 
@@ -68,4 +74,37 @@ async fn configure_database(config: &DatabaseSettings) -> DatabaseConnection {
         .expect("Failed to run migrations");
 
     db_conn
+}
+
+impl TestApp {
+    pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/subscriptions", self.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+        // Extract the link from one of the request fields.
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+            assert_eq!(links.len(), 1);
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+            // Let's make sure we don't call random APIs on the web
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+        let html = get_link(&body["HtmlBody"].as_str().unwrap());
+        let plain_text = get_link(&body["TextBody"].as_str().unwrap());
+        ConfirmationLinks { html, plain_text }
+    }
 }
